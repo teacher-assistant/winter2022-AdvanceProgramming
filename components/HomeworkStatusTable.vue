4f7fa66ffe3bd98f5b69022c01dec5936085d66c
Omid Reza <omid77.orh@gmail.com>
2022-03-16 18:20:07 +0330

    deploy main project into this repository

---
+++ components/HomeworkStatusTable.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="alert alert-dark">
+    	<h3>
+    		Below table is updated <span class="alert-link">a few days after deadlines</span>
+    	</h3>
+    	<div class="table-responsive">
+		<table class="table table-primary table-striped">
+		  <thead>
+		    <tr>
+		      <th scope="col">#</th>
+		      <th scope="col">Student Number</th>
+		      <th scope="col">HW1</th>
+		      <th scope="col">HW2</th>
+		      <th scope="col">HW3</th>
+		      <th scope="col">HW4</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    <tr v-for="(student, index) in students">
+		      <th scope="row">{{ index+1 }}</th>
+		      <td>{{ student.id }}</td> 
+		      <td> {{ student.HW1 }}</td>
+		      <td> {{ student.HW2 }}</td>
+		      <td> {{ student.HW3 }}</td>
+		      <td> {{ student.HW4 }}</td>
+		    </tr>
+		  </tbody>
+		</table>
+    	</div>
+    </div>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			students:[]
+		}
+	},
+  	mounted() {
+	    this.$nextTick(()=>{
+		  	this.$axios.$get('https://teacher-assistant.github.io/winter2022-AdvanceProgramming/HomeWorks.json')
+	        .then(response=>this.students=response)
+	    })
+  	}
+}
+</script>
